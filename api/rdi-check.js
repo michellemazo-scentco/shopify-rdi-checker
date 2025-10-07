bb0ea630fb80471f36ebf4f311fd704782662407
Michelle Mazo <michelle.mazo@scentcoinc.com>
2025-10-07 08:06:43 -0700

    Add shopify store url

--- api/rdi-check.js
+++ api/rdi-check.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     console.log("RDI checker triggered");
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://scentco-fundraising.myshopify.com');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
